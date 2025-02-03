7ef801702c52356e7b995ea0765161687d1a2cdd
asifgenius <asif15-11288@diu.edu.bd>
2025-02-03 08:34:21 +0600

    Add AdminController

---
+++ src/controllers/AdminController.php
@@ -0,0 +1,34 @@
+<?php
+require_once __DIR__ . '/../../src/config/db.php';
+require_once __DIR__ . '/../../src/services/eventService.php';
+require_once __DIR__ . '/../../src/utils/authGaurd.php';
+
+class AdminController
+{
+    public function __construct() {
+        isAuthenticated();
+    }
+
+    public function index(){
+        require_once __DIR__ . '/../views/dashboard.html';
+    }
+    public function dashboard()
+    {
+        $limit = isset($_GET['itemsPerPage']) ? (int)$_GET['itemsPerPage'] : 5;
+        $offset = (isset($_GET['page']) ? (int)$_GET['page'] - 1 : 0) * $limit;
+        $sortBy = isset($_GET['sortBy']) ? $_GET['sortBy'] : 'title';
+        $order = isset($_GET['order']) ? $_GET['order'] : 'ASC';
+        $filter = isset($_GET['filter']) ? $_GET['filter'] : '';
+
+        $eventService = new EventService();
+        $events = $eventService->getEvents($limit, $offset, $sortBy, $order, $filter);
+
+        $totalEvents = $eventService->count();
+        $totalPages = ceil($totalEvents / $limit);
+
+        echo json_encode([
+            'events' => $events,
+            'totalPages' => $totalPages
+        ]);
+    }
+}
